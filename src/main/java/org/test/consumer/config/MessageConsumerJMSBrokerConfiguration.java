6b379e6fb4a70cd06ceb75aee090b34f3139990e
Zoltan Mezei <mezei.zoltan@gmail.com>
2023-11-07 15:08:52 +0100

    Add username/password handling for the message broker factory

--- src/main/java/org/test/consumer/config/MessageConsumerJMSBrokerConfiguration.java
+++ src/main/java/org/test/consumer/config/MessageConsumerJMSBrokerConfiguration.java
@@ -1,9 +1,11 @@
 package org.test.consumer.config;
 
+
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class MessageConsumerJMSBrokerConfiguration {
@@ -11,10 +13,22 @@ public class MessageConsumerJMSBrokerConfiguration {
     @Value("${brokerurl}")
     private String brokerUrl;
 
+    @Value("${brokerusername")
+    private String brokerUserName;
+
+    @Value("${brokerpassword")
+    private String brokerPassword;
+
     @Bean
     public ActiveMQConnectionFactory connectionFactory() {
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
         connectionFactory.setBrokerURL(brokerUrl);
+        if (StringUtils.hasText(brokerUserName)) {
+            connectionFactory.setUserName(brokerUserName);
+        }
+        if (StringUtils.hasText(brokerPassword)) {
+            connectionFactory.setPassword(brokerPassword);
+        }
         connectionFactory.setTrustAllPackages(true);
         return connectionFactory;
     }
